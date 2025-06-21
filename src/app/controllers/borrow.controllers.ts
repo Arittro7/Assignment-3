19ad07f852e9c3d721872e39cb809c285f08483a
Arittro7 <arittrowpdev@gmail.com>
2025-06-21 23:46:35 +0600

    borrow issue

--- src/app/controllers/borrow.controllers.ts
+++ src/app/controllers/borrow.controllers.ts
@@ -1,100 +1,108 @@
-import express, { Request, Response } from 'express'
-import { Book } from '../models/book.model';
-import { Borrow } from '../models/borrow.model';
+import express, { Request, Response } from "express";
+import { Book } from "../models/book.model";
+import { Borrow } from "../models/borrow.model";
 
+export const borrowRoute = express.Router();
 
-export const borrowRoute = express.Router()
-
-borrowRoute.post('/', async (req: Request, res: Response): Promise<any> => {
+borrowRoute.post("/", async (req: Request, res: Response): Promise<any> => {
   try {
-    const {bookId, quantity, dueDate} = req.body;
-    if(!bookId || !quantity || !dueDate){
+    const { bookId, quantity, dueDate } = req.body;
+    if (!bookId || !quantity || !dueDate) {
       return res.status(400).json({
         success: false,
-        message: 'Missing required fields',
-        error: 'book, quantity and dueDate required',
-      })
+        message: "Missing required fields",
+        error: "book, quantity and dueDate required",
+      });
     }
 
-    // quantity validation
-    const book = await Book.findById(bookId)
+    const update = {
+      $inc: { copies: -quantity },
+    };
 
-    if(!book){
-      return res.status(404).json({
-        success: false,
-        message: "Book not found",
-        error: 'Book may not exists or Invalid book id'
-      })
+    const query = {
+      _id: bookId,
+      copies: { $gte: quantity },
+    };
+
+    const options = { new: true };
+    const updatedBook = await Book.findOneAndUpdate(query, update, options);
+
+    if (!updatedBook) {
+      const book = await Book.findById(bookId);
+      if (!book) {
+        return res.status(404).json({
+          success: false,
+          message: "Book not found",
+          error: "Book may not exists or Invalid book id",
+        });
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "Not enough copies available",
+          error: `Only ${book.copies} are left`,
+        });
+      }
     }
 
-    if(book.copies < quantity){
-      return res.status(400).json({
-        success: false,
-        message: 'Not enough copies available',
-        error: `Only ${book.copies} copies are left` 
-      })
-
+    if (updatedBook.copies === 0){
+      updatedBook.available = false
+      await updatedBook.save()
     }
 
-    book.copies -= quantity
-    if(book.copies === 0){
-      book.available = false
-    }
-
-    await book.save();
-    
-    const borrow = await Borrow.create({book : book._id, quantity, dueDate})
+    const borrow = await Borrow.create({
+      book: updatedBook._id,
+      quantity,
+      dueDate,
+    });
 
     res.status(201).json({
       success: true,
-      message: 'Book borrowed successfully',
-      data: borrow
-    })
-
+      message: "Book borrowed successfully",
+      data: borrow,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Failed to Borrow this book"
-    })
+      message: "Failed to Borrow this book",
+    });
   }
-})
+});
 
 // Borrowed books summary
 
-borrowRoute.get('/', async (req: Request, res: Response): Promise<any> => {
+borrowRoute.get("/", async (req: Request, res: Response): Promise<any> => {
   const summary = await Borrow.aggregate([
     {
       $group: {
-        _id: '$book',
-        totalQuantity : {$sum : '$quantity'}
-      }
+        _id: "$book",
+        totalQuantity: { $sum: "$quantity" },
+      },
     },
     {
-      $lookup:{
-        from: 'books',
-        localField: '_id',
-        foreignField: '_id',
-        as: 'bookInfo'
-      }
+      $lookup: {
+        from: "books",
+        localField: "_id",
+        foreignField: "_id",
+        as: "bookInfo",
+      },
     },
     {
-      $unwind : '$bookInfo',
+      $unwind: "$bookInfo",
     },
     {
       $project: {
         _id: 0,
         totalQuantity: 1,
-        book:{
-          title: '$bookInfo.title',
-          isbn: '$bookInfo.isbn'
-        }
-      }
-    }
-  ])
+        book: {
+          title: "$bookInfo.title",
+          isbn: "$bookInfo.isbn",
+        },
+      },
+    },
+  ]);
 
   return res.status(200).json({
     success: true,
     message: "Borrowed books summary retrieved successfully",
-    
-  })
-})
+  });
+});
--- src/app/models/borrow.model.ts
+++ src/app/models/borrow.model.ts
@@ -1,6 +1,5 @@
 import { model, Schema } from 'mongoose';
 import { IBorrow } from './../interfaces/borrow.interface';
-import { Book } from './book.model';
 
 
 const borrowSchema = new Schema<IBorrow>({
@@ -9,14 +8,6 @@ const borrowSchema = new Schema<IBorrow>({
   dueDate: {type: Date, required: true}, 
 },{
   timestamps: true, versionKey: false
-})
-
-borrowSchema.post<IBorrow>("save", async function(){
-  const borrowedBook = await Book.findById(this.book)
-  if(borrowedBook){
-    borrowedBook.copies -= this.quantity
-    await borrowedBook.save()
-  } 
 })
 
 export const Borrow = model<IBorrow>('Borrow', borrowSchema)
