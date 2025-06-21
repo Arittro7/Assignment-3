542045dbcd3c30af980cdd3ca734740797489239
Arittro7 <arittrowpdev@gmail.com>
2025-06-21 11:13:00 +0600

    book schema & model

--- src/app/interfaces/book.interface.ts
+++ src/app/interfaces/book.interface.ts
@@ -1,9 +1,9 @@
 export interface IBook{
   title: string,
   author: string,
-  genre: 'FICTION' | 'NON_FICTION' | 'SCIENCE' | 'HISTORY' | 'BIOGRAPHY' | 'FANTASY' ,
+  genre: 'FICTION' | 'NON_FICTION' | 'SCIENCE' | 'HISTORY' | 'BIOGRAPHY' | 'FANTASY',
   isbn: string,
   description : string,
-  copies: string,
+  copies: number,
   available : boolean 
 }
